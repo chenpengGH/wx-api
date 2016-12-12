37f7f439e94fecfed600c04741376278d4204467
chenpengGH <307410838@qq.com>
2016-12-12 10:50:26 +0800

    token 修改

--- src/main/java/cp/test/wx/wx_api/ACCESS_TOKEN.java
+++ src/main/java/cp/test/wx/wx_api/ACCESS_TOKEN.java
@@ -1,6 +1,6 @@
 package cp.test.wx.wx_api;
 
 public interface ACCESS_TOKEN {
-	String access_token = "JpUiLSMS1uwbnDwMudD1c_r8XeujH_6jMNT4r1dXRVlzcqHFhjOs8u-2Rq2bezCxZkbSa6xz0bPETAEy2w5MHJDmi75yEcjoHXUGRLyrNSiuR22LN40achx6M0Q9Sp37LYZgAIACPQ";
+	String access_token = "5mJMktjdKMUkhyxHNr5Z0uskrKwG3juO9Ityr7Cw5uqd2ll_EmBM-PAF1WvqXP4W-hOy2QWyuOKcG8vfcjlkYopUPzEkml4r1jp7X93ZYkCst3plXj6dKpv3wPwKOtxpTLShAAAPHO";
 }
  
