222634dd1ea628a48839310b1f8e6d456ba21812
Kong <307410838@qq.com>
2017-01-27 21:36:07 +0800

    pom 添加 插件

--- src/main/java/cp/test/wx/wx_api/ACCESS_TOKEN.java
+++ src/main/java/cp/test/wx/wx_api/ACCESS_TOKEN.java
@@ -3,6 +3,7 @@ package cp.test.wx.wx_api;
 import cp.test.wx.wx_api.util.JedisUtil;
 
 public interface ACCESS_TOKEN {
-	String access_token = JedisUtil.getToken();
+//	String access_token = JedisUtil.getToken();
+	String access_token = "A1PcqzJga2kA2Ad6W3ngeDrBcfAU5FNnUou0v50GYKfaKltJL2zyCCxCETEbMRjIPN2PO4cVaSIzGE7_EuX3BuVPyQFADlX6i7BsKVhNyqkGPUaADAKTY";
 }
  
--- src/main/java/cp/test/wx/wx_api/client/TestWXCardget.java
+++ src/main/java/cp/test/wx/wx_api/client/TestWXCardget.java
@@ -18,8 +18,8 @@ import java.util.Map;
 public class TestWXCardget {
 
 	public static void main(String[] args) throws Exception {
-		getCardInfo("pUZWKszrtMDEM-axuB3PkphbpdPs");
-//		getCardInfo("pUZWKs92teC9HZQjDVw7CeVBny2M");
+		getCardInfo("pUZWKs18pGy9Nt_t-QYhEN46R-fo");
+		getCardInfo("pUZWKs-X7HqzKJVhH53v_zfdqm-E");
 //		getCardInfo("pUZWKs4QbnUlWmlxN69Nsex1ZR4c");
 	}
 	
